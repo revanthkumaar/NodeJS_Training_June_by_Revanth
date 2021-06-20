8a524f82ecdc57ba51d5c9976f42725da1b7fdde
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-20 12:55:14 +0530

    asda
    sdasdas

--- first_crud_app_with_express/server.js
+++ first_crud_app_with_express/server.js
@@ -1,5 +1,8 @@
 const expressImport = require('express')
 const serverApp = expressImport();
+const bodyParser = require('body-parser')
+
+serverApp.use(bodyParser.urlencoded({ extended: true }))
 
 serverApp.get('/',function(req,res){
    res.sendFile(__dirname + '/index.html')
@@ -8,6 +11,12 @@ serverApp.get('/',function(req,res){
 serverApp.get('/json',function(req,res){
     res.send('<h1>my first server on express</h1>')
 })
+
+serverApp.post('/userdata',function(req,res){
+    console.log(req.body);
+    res.send('got user info')
+})
+
 
 serverApp.listen(5000,function(){
     console.log('server created on port 5000')
