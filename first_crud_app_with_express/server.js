5085d24fb38c8ebe52bc6c708c7dda5ac91a77c7
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-27 10:50:25 +0530

    updated collection from UI

--- first_crud_app_with_express/server.js
+++ first_crud_app_with_express/server.js
@@ -7,26 +7,39 @@ const connectionString = 'mongodb+srv://revanth:revanth1994@cluster0.z9wuj.mongo
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
 .then(client => {
-    console.log('connected to database')
+    console.log('connected to database');
+ 
+
+    //CREATE DATA - C
+    serverApp.post('/userdata',function(req,res){
+        console.log('inside /userdata call');
+        const db = client.db('crud_database');
+        const usercollection = db.collection('users')
+        console.log(req.body)
+        userCollection.insertOne(req.body)
+        .then(result => {
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    
 })
 .catch(error => console.log(error))
 
 //MongoClient.connect('string',{config}).then().catch()
+
 
 serverApp.use(bodyParser.urlencoded({ extended: true }))
 
 serverApp.get('/',function(req,res){
    res.sendFile(__dirname + '/index.html')
 })
-
 serverApp.get('/json',function(req,res){
     res.send('<h1>my first server on express</h1>')
 })
 
-serverApp.post('/userdata',function(req,res){
-    console.log(req.body);
-    res.send('got user info')
-})
+
 
 
 serverApp.listen(5000,function(){
